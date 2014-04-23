d3859bc0ddec4f7ebfe5439409bb8d4270fd90cd
Võ Hoàng Sang <sangvohoang@vnoss.org>
2014-04-23 15:44:29 +0700

    Fix cant load 1080p
    Fix prevent CMS Talk do anything to prevent this extension work

--- Xeko talkTV HD/content.js
+++ Xeko talkTV HD/content.js
@@ -1,3 +1,5 @@
 var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js";
 script.src = chrome.extension.getURL("my_file.js");
 document.documentElement.appendChild(script);
--- Xeko talkTV HD/my_file.js
+++ Xeko talkTV HD/my_file.js
@@ -1,3 +1,21 @@
-loadPlayer.limit="0";
-loadPlayer.manifestUrl = "http://tv.csmtalk.vn/streaming/play/manifest.smil?channel=pewpewvn&listbitrate=240p:360p:480p:720p&serverIp=live.csmtalk.vcdn.vn&type=livetv&limit=0";
-loadPlayer.initialize();
+$.get( "http://tv.csmtalk.vn/streaming/play/talk-client/channel/pewpewvn?key=Talk%40build", function( data ) {
+  var hd_data = data.split('\n');
+  var limit= "";
+  var manifestUrl="";
+  for (var i in hd_data) {
+	if (limit !== "" && manifestUrl!=="") {
+		break;
+	} 
+	if (hd_data[i].indexOf("loadPlayer.manifestUrl")>-1) {
+		manifestUrl = hd_data[i];
+		continue;
+	}
+	if (hd_data[i].indexOf("loadPlayer.limit") > -1){
+		limit = hd_data[i];
+		continue;
+	}
+  }
+  eval(manifestUrl);
+  eval(limit);
+  loadPlayer.initialize();
+});
